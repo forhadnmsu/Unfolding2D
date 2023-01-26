93d2585d4809afe1939e61c859a5fb2c5eaa6d90
Forhad Hossain <42627490+forhadnmsu@users.noreply.github.com>
2023-01-26 14:32:51 -0600

    removing the duplicate rootfile

--- SplitFile.C
+++ SplitFile.C
@@ -6,16 +6,13 @@ void SplitFile(){
 	auto df2 = ROOT::RDataFrame("result_mc", "../Unfolding1D/Y_4pi_GMC_Jan03_LD2_Dataset1and3.root"); // combined test data1
 	auto df3 = ROOT::RDataFrame("result_mc", "../Unfolding1D/Y_4pi_GMC_Jan03_LD2_Dataset2and4.root"); //combined test data2
 	auto df4 = ROOT::RDataFrame("result_mc", "../Unfolding1D/data/GMC_Nov19_2022_LD2_Acc_DY_67_5to7GeV.root"); // combined test data3
-	auto df5 = ROOT::RDataFrame("result_mc", "../Unfolding1D/combined.root"); // Independent 4pi (from NO target); for acc factor
-	auto df6 = ROOT::RDataFrame("result_mc", "../Unfolding1D/data/GMC_Nov19_2022_LD2_Acc_DY_67_5to7GeV.root");
+	auto df5 = ROOT::RDataFrame("result_mc", "../Unfolding1D/combined.root"); // Independent 4pi (from NONE target); for acc factor
 
 	df1.Filter(cutTrue).Snapshot("result_mc", "data/DY_4pi_GMC_Jan03_LD2_Train.root");  //for response matrix
 	df5.Filter(cutTrue).Snapshot("result_mc", "data/DY_4pi_GMC_4piAccfactor.root");  // denominator 4pi accceptance factor
-
 	df2.Filter(cutRecoMC).Snapshot("result_mc", "data/DY_4pi_GMC_Jan03_LD2_Test1.root");
 	df3.Filter(cutRecoMC).Snapshot("result_mc", "data/DY_4pi_GMC_Jan03_LD2_Test2.root");
 	df4.Filter(cutRecoMC).Snapshot("result_mc", "data/DY_4pi_GMC_Jan03_LD2_Test3.root");
-	df6.Filter(cutRecoMC).Snapshot("result_mc", "data/DY_4pi_GMC_Jan03_LD2_Test4.root");
 
 	//Splitting the realData
 	auto df_unmix = ROOT::RDataFrame("result", "/Users/forhadhossain/SeaQuest/Unfold/Unfolding/Unfolding2D/data/AllData/merged_RS67_3089LD2.root");
